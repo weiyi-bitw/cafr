549565e1fe33b7aa8a68df6865fb9663a48c9862
weiyi <ninpy.weiyi@gmail.com>
2012-10-26 17:34:32 -0400

    load.clnc

--- R/exprFileMisc.R
+++ R/exprFileMisc.R
@@ -23,6 +23,29 @@ load.exp <- function(file, sep='\t'){
 	mset
 }
 
+load.clnc <- function(file, sep='\t'){
+	line = readLines(file)
+	tokens = strsplit(line[1], sep)[[1]]
+	n = length(tokens)-1
+	m = length(line)-1
+	mset = matrix(NA, m, n)	
+	cname = tokens[2:(n+1)]
+	rname = rep(NA, m)
+	b = txtProgressBar(style=3)
+	for(i in 1:m){
+		tokens = strsplit(line[i+1], sep)[[1]]
+		tokens[tokens=="[Not Available]"] = NA
+		rname[i] = tokens[1]
+		mset[i,] = tokens[2:(n+1)]
+		if(i %% 100 == 0){
+			setTxtProgressBar(b, i/m)
+		}
+	}
+	cat("\n")
+	colnames(mset) <- cname
+	rownames(mset) <- rname
+	mset
+}
 # load text mat, such as clinical file
 loadTextMat <- function(file, sep="\t"){
 	mset <- read.table(file=file, sep=sep, as.is=TRUE)
