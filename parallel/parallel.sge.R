2990d34e4130bfc469f9443d6b7a63a62881ca5d
Wei-Yi Cheng <ninpy.weiyi@gmail.com>
2013-03-06 08:11:26 -0500

    add shell script, update parallel.sge.R (originally parallel.ec2.R)

---
+++ parallel/parallel.sge.R
@@ -0,0 +1,56 @@
+# R script for Amazon EC2 cluster
+#
+# arguments for script:
+#	args[1] : input gene expression data matrix
+#	args[2] : worker id
+#	args[3] : total workers
+#	args[4] : job codename (optional)
+#
+#
+
+require(devtools)
+require(parallel)
+require(caret)
+require(RCurl)
+
+# Install cafr package
+install_github(repo="cafr", username="weiyi-bitw", ref="dev")
+library(cafr)
+
+args <- commandArgs(TRUE)
+
+#================================
+# Load data
+#===============================
+
+load(args[1])
+codename = args[1]
+wid = 1
+numWorkers=1
+if(length(args)>1){
+	wid = as.numeric(args[2])+1
+	numWorkers = as.numeric(args[3])
+}
+if(length(args)>3){
+	codename = args[4]
+}
+
+cat("=======================================\n")
+cat("JOB NAME:\t", codename, "\n", sep="")
+cat("FILE NAME:\t", args[1], "\n", sep="")
+cat("WID:\t", wid, "\n",sep="")
+cat("TOTAL WORKERS:\t",  numWorkers, "\n", sep="")
+cat("=======================================\n")
+flush.console()
+
+m = nrow(ge)
+taskList = createFolds(1:m, k=numWorkers)
+
+cat("Finding attractors...\n");flush.console()
+
+as = parAttractorScanning(ge, taskList=taskList, wid=wid)
+save(as, file=paste(codename,".attractors.", sprintf("%04d", wid), ".rda", sep=""))
+
+cat("Done.\n")
+cat("=======================================\n");flush.console()
+
