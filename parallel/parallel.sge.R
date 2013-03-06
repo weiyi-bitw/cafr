fa341c706497000413b196033699f3de0c4beb25
Wei-Yi Cheng <ninpy.weiyi@gmail.com>
2013-03-06 09:44:44 -0500

    create dir when output

--- parallel/parallel.sge.R
+++ parallel/parallel.sge.R
@@ -27,12 +27,14 @@ load(args[1])
 codename = args[1]
 wid = 1
 numWorkers=1
+dirname = "output"
 if(length(args)>1){
 	wid = as.numeric(args[2])+1
 	numWorkers = as.numeric(args[3])
 }
 if(length(args)>3){
 	codename = args[4]
+	dirname=codename
 }
 
 cat("=======================================\n")
@@ -44,12 +46,14 @@ cat("=======================================\n")
 flush.console()
 
 m = nrow(ge)
+set.seed(913503)
 taskList = createFolds(1:m, k=numWorkers)
 
 cat("Finding attractors...\n");flush.console()
 
 as = parAttractorScanning(ge, taskList=taskList, wid=wid)
-save(as, file=paste(codename,".attractors.", sprintf("%04d", wid), ".rda", sep=""))
+dir.create(dirname)
+save(as, file=paste(dirname,"/",codename,".attractors.", sprintf("%04d", wid), ".rda", sep=""))
 
 cat("Done.\n")
 cat("=======================================\n");flush.console()
