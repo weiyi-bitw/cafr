c7d825d57c81ec624d5a92894ae14a1c1801efc5
weiyi_gisl <wc2302@columbia.edu>
2013-03-12 17:27:48 -0400

    20130312 1730 work progress

--- R/Attractor.R
+++ R/Attractor.R
@@ -8,13 +8,18 @@
 
 Attractor <- setRefClass(
 	Class="Attractor",
-	fields=list(genes = "numeric",
+	fields=list(
+		id = "character",
+		genes = "numeric",
+		strength = "numeric",
 		src = "character"),
 	methods=list(
-		initialize = function(a, genenames, src, numgenes){
+		initialize = function(id, a, genenames, src, numgenes, qt){
+			.self$id <- id
 			o <- order(a, decreasing=TRUE)[1:numgenes]
 			genes <<- a[o]
 			names(genes) <<- genenames[o]
+			strength <<- genes[qt]
 			src <<- src
 			return (.self)
 		},
@@ -23,7 +28,15 @@ Attractor <- setRefClass(
 				t <- table(c(names(.self$genes), names(a$genes)))
 				return (sum(t[t>1]))
 			}else if(class(a) == "AttractorSet"){
-				
+				allgenes <- names(.self$genes)
+				allgenes <- c(allgenes, unlist(lapply(a$attractors, 
+							function(aa){
+								if(aa$src == .self$src){ return (NULL) }
+								else return (names(aa$genes))
+							}))
+					)
+				t <- table(allgenes)
+				return (sum(t[t>1]))
 			}
 		}
 	)
--- R/AttractorSet.R
+++ R/AttractorSet.R
@@ -20,9 +20,56 @@ AttractorSet <- setRefClass(
 			return(.self)
 		},
 		add = function(a){
-			attractors[[length(attractors)+1]]<<- a
-			names(attractors)[[length(attractors)]] = a$src
+			if(class(a) == "Attractor"){
+				attractors[[length(attractors)+1]]<<- a
+				names(attractors)[[length(attractors)]] <<- a$src
+			}else if(class(a)=="AttractorSet"){
+				for(aa in a$attractors){
+					if(is.null(.self$attractors[[aa$src]])){
+						attractors[[aa$src]] <<- aa
+					}else{
+						myaa <- .self$attractors[[aa$src]]
+						if(myaa$strength < aa$strength){
+							attractors[[aa$src]] <<- aa
+						}
+					}
+				}
+			}
+		},
+		getOverlapNum = function(a){
+			if(class(a)=="Attractor"){
+				allgenes <- names(a$genes)
+				allgenes <- c(allgenes, unlist(lapply(.self$attractors, 
+						function(aa){
+							if(aa$src == a$src){ return (NULL) }
+							else return (names(aa$genes))
+						}))
+						)
+				t <- table(allgenes)
+				return (sum(t[t>1]))
+			}else if(class(a) == "AttractorSet"){
+				k <- lapply(.self$attractors, function(aa){aa$getOverlapNum(a)})
+				return (sum(unlist(k)))
+			}
+		},
+		getGeneTable = function(...){
+			allgenes <- unlist(lapply(.self$attractors), function(aa){names(aa$genes)})
+			return (sort(table(allgenes), decreasing=TRUE))
+		},
+		getConsensus = function(sz){ # NOT REAL CONSENSUS, ONLY THE CONSENSUS FROM TOP GENES
+			allgenes <- unlist(lapply(.self$attractors), function(aa){names(aa$genes)})
+			g <- unique(allgenes)
+			mat <- matrix(0, nrow=length(g), ncol=.self$capacity)
+			rownames(mat) <- g
+			colnames(mat) <- unlist(lapply(.self$attractors, function(aa){aa$src}))
+			for(aa in .self$attractors){
+				gg <- names(aa$genes)
+				mat[gg, aa$src] <- aa$genes
+			}
+			mis <- apply(mat, 1, mean)
+			return (sort(mat, decreasing=TRUE)[1:sz])
 		}
+		
 	)
 
 )
