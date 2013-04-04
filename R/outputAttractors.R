bd03984599dd968d72be5c665cdf64343fcbef91
weiyi_gisl <wc2302@columbia.edu>
2013-04-04 14:48:17 -0400

    revise the format of outputAttractors.R

--- R/outputAttractors.R
+++ R/outputAttractors.R
@@ -8,8 +8,9 @@ outputAttractors <- function(x, min.basins=2, strength.pos=10, outputGeneNumber=
 		outMat <- cbind(outMat,colnames(x)[o[1:outputGeneNumber]], round(x[i,o[1:outputGeneNumber]], 4))
 		aname <- paste("Attractor", sprintf("%03d",k), sep="")
 		colnames(outMat)[c(2*k-1, 2*k)] <- c(paste(aname, ":Genes",sep="" ), paste(aname, ":MI",sep="") )
-		k = k+1
+		k <- k+1
 	}
+	rownames(outMat) <- 1:outputGeneNumber
 	if(write2File){
 		write.table(outMat, file=fileName, sep='\t', quote=F)
 	}
