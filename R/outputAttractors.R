d22c37526aa04dc8572a7c34deac26def7cd9461
weiyi_gisl <wc2302@columbia.edu>
2013-04-04 14:23:54 -0400

    add outputAttractors function

---
+++ R/outputAttractors.R
@@ -0,0 +1,17 @@
+outputAttractors <- function(x, min.basins=2, strength.pos=10, outputGeneNumber=100, write2File=FALSE, fileName="attractors.txt"){
+	m <- nrow(x)
+	outMat <- NULL
+	k <- 1
+	for(i in 1:m){
+		o <- order(x[i,], decreasing=TRUE)
+		if(rownames(x)[i] %in% colnames(x)[o[1:min.basins]]) next
+		outMat <- cbind(outMat,colnames(x)[o[1:outputGeneNumber]], round(x[i,o[1:outputGeneNumber]], 4))
+		aname <- paste("Attractor", sprintf("%03d",k), sep="")
+		colnames(outMat)[c(2*k-1, 2*k)] <- c(paste(aname, ":Genes",sep="" ), paste(aname, ":MI",sep="") )
+		k = k+1
+	}
+	if(write2File){
+		write.table(outMat, file=fileName, sep='\t', quote=F)
+	}
+	return (outMat)
+}
