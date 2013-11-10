b214f52e99dd3bef0178368d9c80f2e484b30060
weiyi-bitw <wc2302@columbia.edu>
2013-11-09 21:58:00 -0500

    even less trimming

--- R/parAttractorScanning.R
+++ R/parAttractorScanning.R
@@ -27,7 +27,7 @@ parAttractorScanning <- function(data, taskList=list(1:nrow(data)), wid=1,  a=5,
           max(abs(x - out)) > 1E-4
         })
         if(prod(un) == 0){
-          killIdx <- which(out >= out[i])
+          killIdx <- which(out >= out[i] & out > 0.5)
           task <- setdiff(task, killIdx)
           rownames(as)[which(un==0)] <- genes[i]
           #if(verbose) {cat("done!\n");flush.console()}
