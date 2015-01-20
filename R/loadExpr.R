1ff6f5d484c5691087ba7d98a4c75b472ae5c253
weiyi-bitw <ninpy.weiyi@gmail.com>
2015-01-20 12:14:24 -0500

    fix bug in loadExpr.R where sep does not actually do anything

--- R/loadExpr.R
+++ R/loadExpr.R
@@ -1,6 +1,6 @@
 loadExpr <- function(file, sep='\t'){
   line <- readLines(file)
-  tokens <- strsplit(line[1], "\t")[[1]]
+  tokens <- strsplit(line[1], sep)[[1]]
   n <- length(tokens)-1
   m <- length(line)-1
   mset <- matrix(NA, m, n)	
@@ -8,7 +8,7 @@ loadExpr <- function(file, sep='\t'){
   rname <- rep(NA, m)
   b <- txtProgressBar(style=3)
   for(i in 1:m){
-    tokens <- strsplit(line[i+1], "\t")[[1]]
+    tokens <- strsplit(line[i+1], sep)[[1]]
     tokens[tokens=="NA"] <- NA
     rname[i] <- tokens[1]
     mset[i,] <- as.numeric(tokens[2:(n+1)])
